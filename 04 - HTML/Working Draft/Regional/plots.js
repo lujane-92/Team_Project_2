779dda180934ebb2016a425fb3f62c5d5db783a7
Lujane Abdelgadir <lujane-92@users.noreply.github.com>
2020-06-11 15:51:21 -0400

    Updated HTML

--- 04 - HTML/Working Draft/Regional/plots.js
+++ 04 - HTML/Working Draft/Regional/plots.js
@@ -4,83 +4,66 @@ var trace = {
   y: [1340598447, 4294516659, 834995197, 658345826, 260991690, 368869647, 42690838],
   type: "bar"
 };
+var trace1 = {
+  x: ["Africa","Asia","Europe", "Latin America / Caribbean", "Middle East","North America", "Australia"],
+  y: [526710313, 2366213308, 727848547, 453702292, 183212099, 348908868, 28917600],
+  type: "bar"
+};
 
-var data = [trace];
+var data = [trace, trace1];
 
 var layout = {
-  title: "Population by region",
-  xaxis: { title: "Region"},
-  yaxis: { title: "Popuation"}
+  paper_bgcolor : 'rgb(78,93,108)',
+  plot_bgcolor: 'rgb(78,93,108)',
+  title: {
+      text: "Population by region",
+      font: {color: 'rgb(255,255,255)'}
+  },
+  yaxis:{
+      title: 'Population',
+      color:'rgb(255,255,255)'
+  },
+  xaxis:{
+      title: 'Regions',
+      color:'rgb(255,255,255)'},
+  
+  legend: {
+    font: {
+       color: 'white'
+     }},
+
 };
 
 Plotly.newPlot("plot", data, layout);
 
-
-// // Part 2 - Adding attributes
-// var trace1 = {
-//   x: ["beer", "wine", "martini", "margarita",
-//       "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-//   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//   type: "bar"
-// };
-
-// var data = [trace1];
-
-// var layout = {
-//   title: "'Bar' Chart",
-//   xaxis: { title: "Drinks"},
-//   yaxis: { title: "% of Drinks Ordered"}
-// };
-
-// Plotly.newPlot("plot", data, layout);
-
-
-// // Part 3 - Line Chart
-// var trace1 = {
-//   x: ["beer", "wine", "martini", "margarita",
-//       "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-//   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//   type: "line"
-// };
-
-// var data = [trace1];
-
-// var layout = {
-//   title: "'Bar' Chart",
-// };
-
-// Plotly.newPlot("plot", data, layout);
-
-// // Part 4 - Broken Pie Chart
-// var trace1 = {
-//   x: ["beer", "wine", "martini", "margarita",
-//       "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-//   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//   type: "pie"
-// };
-
-// var data = [trace1];
-
-// var layout = {
-//   title: "'Bar' Chart",
-// };
-
-// Plotly.newPlot("plot", data, layout);
 
 
 // // Part 5 - Working Pie Chart
  var trace1 = {
    labels: ["Africa","Asia","Europe", "Latin America / Caribbean", "Middle East", "North America", "Australia"],
    values: [17.20, 55.10, 10.70, 8.50, 3.90, 4.70, 0.50],
+   textfont: {
+     color: 'white'},
    type: 'pie'
  };
 
  var data1 = [trace1];
 
  var layout = {
-   title: "% of Population by Region",
-   legend: {x: 0, y: -2}
- };
+    paper_bgcolor : 'rgb(78,93,108)',
+    plot_bgcolor: 'rgb(78,93,108)',
+    title: {
+        text: "% of Population by Region",
+        font: {color: 'rgb(255,255,255)'}
+    },
+    
+    legend: {
+      x: 0, y: -2,
+      font: {
+         color: 'white'
+       }},
+
+};
 
  Plotly.newPlot("plot2", data1, layout);
 
